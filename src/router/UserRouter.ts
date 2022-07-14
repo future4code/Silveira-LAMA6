474eb4156dc1cadd23eb38aea1c02ce2217cd613
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-07-14 17:51:24 -0300

    Projeto Lama

---
+++ src/router/UserRouter.ts
@@ -0,0 +1,27 @@
+import express from "express"
+import { UserBusiness } from "../Business/UserBusiness";
+
+import UserController from "../Controller/UserController";
+import UserData from "../Data/UserDataBase";
+import hashGenerator, { HashGenerator } from "../Services/hashGenerator";
+import { idGenerator } from "../Services/idGenerator";
+import { TokenGenerator } from "../Services/tokenGenerator";
+
+const userBusiness = new UserBusiness(
+    new UserData(),
+    new idGenerator(),
+    new HashGenerator(),
+    new TokenGenerator()
+)
+
+const userController = new UserController(
+    userBusiness
+)
+
+export const userRouter = express.Router();
+
+
+userRouter.post("/signup", userController.signup)
+userRouter.post("/login", userController.login)
+
+
